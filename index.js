cd22bbe1432979326336b87b2b01b4fc503b02f6
PotatoPost <postalex11@icloud.com>
2021-01-26 15:01:55 +0000

    added comments

--- index.js
+++ index.js
@@ -4,9 +4,17 @@ const testFile = require("./file"); //USE THIS TO IMPORT OTHER STUFF YOU HECKERS
 // stanleyulili.com/node/node-modules-import-and-use-functions-from-another-file/
 const scanner = require('prompt-sync')({sigint: true}); //USE THIS TO GET INPUT. NIFTY URL: https://www.codecademy.com/articles/getting-user-input-in-node-js
 
+//uses the testFile function thingy
 testFile.whatAmIDoing();
+
+//this just displays stuff to the screen, if you dunno how to do that
 console.log("I forget how to do JS");
+
+//first, ask the user whats 9 + 10
 var num = Number(scanner("What's nine plus ten? "));
+
+//select an outcome based on what they inputed
+//i will probably make this a switch statement later lol
 if (num == 21){
   console.log("You stupid");
 } else if (num == 19) {
