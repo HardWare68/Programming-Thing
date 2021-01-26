8dd774bd38d1393598be6e23b857fe76373ff1ea
PotatoPost <postalex11@icloud.com>
2021-01-26 03:05:24 +0000

    add file.js

---
+++ file.js
@@ -0,0 +1,7 @@
+//this function prints stuff to the screen
+function whatAmIDoing(){
+  console.log("lmaoooo");
+}
+
+//export this bad boy
+module.exports = { whatAmIDoing };
--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+// /!\ please comment all your stuff please :^) /!\
+
+const testFile = require("./file"); //USE THIS TO IMPORT OTHER STUFF YOU HECKERS. FOLLOW THE EXAMPLE, OR THIS NIFTY URL:
+// stanleyulili.com/node/node-modules-import-and-use-functions-from-another-file/
+
+testFile.whatAmIDoing();
 console.log("I forget how to do JS");
