5add52280c68b449edbad9e36cfbc34cddcb5587
PotatoPost <postalex11@icloud.com>
2021-01-26 14:56:38 +0000

    added prompt-sync

--- index.js
+++ index.js
@@ -2,6 +2,15 @@
 
 const testFile = require("./file"); //USE THIS TO IMPORT OTHER STUFF YOU HECKERS. FOLLOW THE EXAMPLE, OR THIS NIFTY URL:
 // stanleyulili.com/node/node-modules-import-and-use-functions-from-another-file/
+const scanner = require('prompt-sync')({sigint: true}); //USE THIS TO GET INPUT. NIFTY URL: https://www.codecademy.com/articles/getting-user-input-in-node-js
 
 testFile.whatAmIDoing();
 console.log("I forget how to do JS");
+var num = Number(scanner("What's nine plus ten? "));
+if (num == 21){
+  console.log("You stupid");
+} else if (num == 19) {
+  console.log("That is correct");
+} else {
+  console.log("No");
+}
