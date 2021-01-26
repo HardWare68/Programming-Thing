2059810253b23ec63d2b86adad2ae374bda83d97
PotatoPost <postalex11@icloud.com>
2021-01-26 16:37:00 +0000

    added a loop

--- index.js
+++ index.js
@@ -2,23 +2,34 @@
 
 const testFile = require("./file"); //USE THIS TO IMPORT OTHER STUFF YOU HECKERS. FOLLOW THE EXAMPLE, OR THIS NIFTY URL:
 // stanleyulili.com/node/node-modules-import-and-use-functions-from-another-file/
+
 const scanner = require('prompt-sync')({sigint: true}); //USE THIS TO GET INPUT. NIFTY URL: https://www.codecademy.com/articles/getting-user-input-in-node-js
 
-//uses the testFile function thingy
-testFile.whatAmIDoing();
+var continueLoop = true;
 
-//this just displays stuff to the screen, if you dunno how to do that
-console.log("I forget how to do JS");
+// main loop of the program
+//its gonna loop everything in here while they say true
+//otherwhise, its gonna exit the loop and terminate the program
+while(String(continueLoop).toLowerCase() == "true"){
+  //uses the testFile function thingy
+  testFile.whatAmIDoing();
 
-//first, ask the user whats 9 + 10
-var num = Number(scanner("What's nine plus ten? "));
+  //this just displays stuff to the screen, if you dunno how to do that
+  console.log("I forget how to do JS");
 
-//select an outcome based on what they inputed
-//i will probably make this a switch statement later lol
-if (num == 21){
-  console.log("You stupid");
-} else if (num == 19) {
-  console.log("That is correct");
-} else {
-  console.log("No");
+  //first, ask the user whats 9 + 10
+  var num = Number(scanner("What's nine plus ten? "));
+
+  //select an outcome based on what they inputed
+  //i will probably make this a switch statement later lol
+  if (num == 21){
+    console.log("You stupid");
+  } else if (num == 19) {
+    console.log("That is correct");
+  } else {
+    console.log("No");
+  }
+
+  continueLoop = scanner('Do you wish to continue? Enter "true" or "false."');
+  console.log(continueLoop);
 }
