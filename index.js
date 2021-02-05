c72563c689ac37e4fa01357ebab5dba61c65f2ab
PotatoPost <postalex11@icloud.com>
2021-02-05 17:51:30 +0000

    tidied some stuff up

--- index.js
+++ index.js
@@ -12,53 +12,12 @@ var continueLoop = true;
 //its gonna loop everything in here while they say true
 //otherwhise, its gonna exit the loop and terminate the program
 while (String(continueLoop).toLowerCase() == "true") {
-  //uses the testFile function thingy
-  testFile.whatAmIDoing();
-  testFile.testStuffAgain();
 
   rolimonStuff.rolimonRequest();
 
-  //first, ask the user whats 9 + 10
-  var num = Number(scanner("What's nine plus ten? "));
+  testFile.ninePlusTen();
 
-  //select an outcome based on what they inputed
-  switch (num) {
-    case 19:
-      console.log("Correct");
-      break;
-
-    case 21:
-      console.log("You stupid");
-      break;
-
-    case 87:
-      console.log("WAS THAT THE BITE OF 87");
-      break;
-
-    default:
-      console.log("no");
-  }
-
-  //ask the user which is heavier
-  var weight = String(scanner("Which is heavier: a pound of steel or a pound of feathers? "));
-
-  //what they put in
-  switch (weight) {
-    case "feathers":
-      console.log("idiot");
-      break;
-    
-    case "steel":
-      console.log("idiot");
-      break;
-    
-    case "neither":
-      console.log("good job");
-      break;
-
-    default:
-      console.log("no");
-  }
+  testFile.steelOrFeathers();
 
   //ask if they wanna continue or not
   continueLoop = scanner('Do you wish to continue? Enter "true" or "false." ');
