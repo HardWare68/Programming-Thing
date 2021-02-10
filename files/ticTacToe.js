e7a0a14b0bbe3a6915de9abd0710887f8d0606eb
HardWare <postalex11@icloud.com>
2021-02-10 04:57:40 +0000

    added tictactoe

--- files/ticTacToe.js
+++ files/ticTacToe.js
@@ -3,36 +3,151 @@
 var gameBoard = [["", "", ""], ["", "", ""], ["", "", ""]]; //this fella right here is a 2-D array
 var isLegalMoveVar = false; //is it a legal move? (it's not legal if they try to go to spot [10,10] or if they go in a spot that is already taken)
 var isBoardFullVar = false; //is game won?
-var isgameWonVar = false; //is the game won?
+var isGameWonVar = false; //is the game won?
 var playerTurn = 1; //which player is going
 var nextMove = [0, 0]; //the next move
 
 const scanner = require('prompt-sync')({ sigint: true });
 
-while(isGameWonVar){
-  //print the board
-
-  //find out who's turn it is
-
-  //loop until they give us a legal move
-  while(!isLegalMoveVar){
-    //prompt user for input
-
-    //get the next move, and hopefully split it into an array
-    //with correct regex. how the hell do i even regex????
-
-    //check if it's a legal move
-
-    //if it is not legal, let them know they did a bad
-  }
-
-  //do the move. you know, put it to the gameBorad array or whatever
-
-  //check if anyone has won
-
-  //check if the board is full
-
-  //reset the legality stuff because otherwise it gets stuck lol
+function printBoard(){
+  console.log(gameBoard[0][0] + "|" + gameBoard[0][1] + "|" + gameBoard[0][2] + "|");
+  console.log("--------");
+  console.log(gameBoard[1][0] + "|" + gameBoard[1][1] + "|" + gameBoard[1][2] + "|");
+  console.log("--------");
+  console.log(gameBoard[2][0] + "|" + gameBoard[2][1] + "|" + gameBoard[2][2] + "|");
 }
 
-//somebody won!
+function isLegalMove(nextMoveParam){
+  try{
+    if(gameBoard[nextMoveParam[0]][nextMoveParam[1]] == ""){
+      isLegalMoveVar = true;
+    } else {
+      isLegalMoveVar = false;
+    }
+  } catch (ArrayIndexOutOfBoundsException){
+    isLegalMoveVar = false;
+  }
+}
+
+function isBoardFull(){
+  if(gameBoard[0][0] != "" && gameBoard[0][1] != "" && gameBoard[0][2] != "" && gameBoard[1][0] != "" && gameBoard[1][1] != "" && gameBoard[1][2] != "" && gameBoard[2][0] != "" && gameBoard[2][1] != "" && gameBoard[2][2] != ""){
+    isBoardFullVar = true;
+  } else {
+    isBoardFullVar = false;
+  }
+}
+
+function isGameWonFunction(){
+    //if first row is equal
+    if(gameBoard[0][0] == gameBoard[0][1] && gameBoard[0][1] == gameBoard[0][2] && gameBoard[0][0] == gameBoard[0][2] && gameBoard[0][0] != "" && gameBoard[0][1] != "" && gameBoard[0][2] != ""){
+      isGameWonVar = true;
+    }
+    //if second row is equal
+    else if(gameBoard[1][0] == gameBoard[1][1] && gameBoard[1][1] == gameBoard[1][2] && gameBoard[1][0] == gameBoard[1][2] && gameBoard[1][0] != "" && gameBoard[1][1] != "" && gameBoard[1][2] != ""){
+      isGameWonVar = true;
+    }
+    //if third row is equal
+    else if(gameBoard[2][0] == gameBoard[2][1] && gameBoard[2][1] == gameBoard[2][2] && gameBoard[2][0] == gameBoard[2][2] && gameBoard[2][0] != "" && gameBoard[2][1] != "" && gameBoard[2][2] != ""){
+      isGameWonVar = true;
+    }
+    //if first column is equal
+    else if(gameBoard[0][0] == gameBoard[1][0] && gameBoard[1][0] == gameBoard[2][0] && gameBoard[0][0] == gameBoard[2][0] && gameBoard[0][0] != "" && gameBoard[1][0] != "" && gameBoard[2][0] != ""){
+      isGameWonVar = true;
+    }
+    //if second column is equal
+    else if(gameBoard[0][1] == gameBoard[1][1] && gameBoard[1][1] == gameBoard[2][1] && gameBoard[0][1] == gameBoard[2][1] && gameBoard[0][1] != "" && gameBoard[1][1] != "" && gameBoard[2][1] != ""){
+      isGameWonVar = true;
+    }
+    //if third column is equal
+    else if(gameBoard[0][2] == gameBoard[1][2] && gameBoard[1][2] == gameBoard[2][2] && gameBoard[0][2] == gameBoard[2][2] && gameBoard[0][2] != "" && gameBoard[1][2] != "" && gameBoard[2][2] != ""){
+      isGameWonVar = true;
+    }
+    //if top-left to bottom-right diagonal is equal
+    else if(gameBoard[0][0] == gameBoard[1][1] && gameBoard[1][1] == gameBoard[2][2] && gameBoard[0][0] == gameBoard[2][2] && gameBoard[0][0] != "" && gameBoard[1][1] != "" && gameBoard[2][2] != ""){
+      isGameWonVar = true;
+    }
+    //if top-right to bottom-left diagonal is equal
+    else if(gameBoard[0][2] == gameBoard[1][1] && gameBoard[1][1] == gameBoard[2][0] && gameBoard[0][2] == gameBoard[2][0] && gameBoard[0][2] != "" && gameBoard[1][1] != "" && gameBoard[2][0] != ""){
+      isGameWonVar = true;
+    }
+    else{
+      isGameWonVar = false;
+    }
+    //im so sorry to all my programming friends who had to scroll through a thousand if statements
+  }
+function ticTacToe(){
+  while(isGameWonVar){
+    //print the board
+    printBoard();
+
+    //find out who's turn it is
+    if(playerTurn == 1){
+      console.log("It is player 1's turn!");
+    } else {
+      console.log("It is player 2's turn!");
+    }
+
+    //loop until they give us a legal move
+    while(!isLegalMoveVar){
+      //prompt user for input
+      var nextMoveOne = scanner("Enter your move! (Format it like so: \"row,column\")").split("[,]");
+
+      //get the next move, and hopefully split it into an array
+      //with correct regex. how the hell do i even regex????
+        try {
+          nextMove[0] = Integer.parseInt(nextMoveOne[0]);
+          nextMove[1] = Integer.parseInt(nextMoveOne[1]);
+        } catch(e) {
+          if (e instanceof ArrayIndexOutOfBoundsException) {
+            isLegalMoveVar = false;
+            continue;
+          } else if (e instanceof NumberFormatException) {
+            isLegalMoveVar = false;
+            continue;
+          }
+        }
+
+      //check if it's a legal move
+      isLegalMove(nextMove);
+
+      //if it is not legal, let them know they did a bad
+      if(isLegalMoveVar == false){
+        console.log("That is an illegal move. Please enter a legal move.")
+      }
+    }
+
+    //do the move. you know, put it to the gameBorad array or whatever
+    if(playerTurn == 1){
+      gameBoard[nextMove[0]][nextMove[1]] = "O";
+      playerTurn = 2;
+    } else {
+      gameBoard[nextMove[0]][nextMove[1]] = "X";
+      playerTurn = 1;
+    }
+
+    //check if anyone has won
+    isGameWon();
+
+    //check if the board is full
+    isBoardFull();
+    if(isBoardFullVar){
+      break;
+    }
+
+    //reset the legality stuff because otherwise it gets stuck lol
+    isLegalMoveVar = false;
+  }
+
+  //somebody won!
+  if(!isBoardFullVar){
+    if(playerTurn == 1){
+      console.log("Player 2 has won!");
+    } else {
+      console.log("Player 1 has won!");
+    }
+  }else{
+    console.log("Draw!");
+  }
+}
+
+module.exports = {ticTacToe};
--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const testFile = require("./files/file"); //USE THIS TO IMPORT OTHER STUFF YOU HECKERS. FOLLOW THE EXAMPLE, OR THIS NIFTY URL:
 // stanleyulili.com/node/node-modules-import-and-use-functions-from-another-file/
 const rolimonStuff = require("./files/rolimonStuff");
+const ticTacToe = require("./files/ticTacToe");
 
 const scanner = require('prompt-sync')({ sigint: true }); //USE THIS TO GET INPUT. NIFTY URL: https://www.codecademy.com/articles/getting-user-input-in-node-js
 
@@ -18,6 +19,8 @@ while (String(continueLoop).toLowerCase() == "true") {
   testFile.ninePlusTen();
 
   testFile.steelOrFeathers();
+
+  ticTacToe.ticTacToe();
 
   //ask if they wanna continue or not
   continueLoop = scanner('Do you wish to continue? Enter "true" or "false." ');
