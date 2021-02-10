2a08304472524eecf03c5c8b1031f1389fc1b95d
BlaisinX <74415050+BlaisinX@users.noreply.github.com>
2021-02-10 08:28:36 -0500

    Add files via upload

--- files/file.js
+++ files/file.js
@@ -1,50 +1,12 @@
-const scanner = require('prompt-sync')({ sigint: true });
-
-function ninePlusTen(){
-  //first, ask the user whats 9 + 10
-  var num = Number(scanner("What's nine plus ten? "));
-
-  //select an outcome based on what they inputed
-  switch (num) {
-    case 19:
-      console.log("Correct");
-      break;
-
-    case 21:
-      console.log("You stupid");
-      break;
-
-    case 87:
-      console.log("WAS THAT THE BITE OF 87");
-      break;
-
-    default:
-      console.log("no");
-  }
+//this function prints stuff to the screen
+function whatAmIDoing(){
+  console.log("lmaoooo");
 }
 
-function steelOrFeathers(){
-  //I got a question for you: what's heavier, a kilogram o steel o a kilogram o feathers?
-  var weight = String(scanner("Which is heavier: a pound of steel or a pound of feathers? ")).toLowerCase();
-
-  //what they put in
-  switch (weight) {
-    case "feathers":
-      console.log("idiot");
-      break;
-    
-    case "steel":
-      console.log("That's right. It's a kilogram o steel, because steel is heavier than feathers!");
-      break;
-    
-    case "neither":
-      console.log("good job");
-      break;
-
-    default:
-      console.log("no");
-  }
+//test stuff again
+function testStuffAgain(){
+  console.log("Hello #alex-post-archives!");
 }
 
 //export this bad boy
-module.exports = {ninePlusTen, steelOrFeathers};
+module.exports = { whatAmIDoing, testStuffAgain };
