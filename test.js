a3571f42558c9324dc03d3ab1282919ae3d45d01
HardWare <postalex11@icloud.com>
2021-02-22 16:47:59 +0000

    fixed syntax error, plus added test logging

--- test.js
+++ test.js
@@ -2,8 +2,11 @@ const testFile = require("./files/javascript/file");
 var testing = testFile.readJSON()
 
 //test steelOrFeathers
-for(var x = 0; x > testing.sOFTesting.inputs.length; x++;) {
+for(var x = 0; x > testing.sOFTesting.inputs.length; x++) {
   if(steelOrFeathers(testing.SOFTesting.inputs[x]) != steelOrFeathers(testing.SOFTesting.outputs[x])){
+    console.log("Test condition " + testing.SOFTesting.inputs[x] + " failed!");
     testFile.intentionallyBreak();
+  } else {
+    console.log("Test condition " + testing.SOFTesting.inputs[x] + " passed!");
   }
 }
